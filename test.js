61831806f3d922aa845cd09c676896a96976b89c
Andrey Dubrovskiy <62926789+aadubrovskij@users.noreply.github.com>
2020-12-11 00:43:42 +0600

    Create test.js

---
+++ test.js
@@ -0,0 +1,13 @@
+const MODULE_NAME = "SubmoduleA"
+
+console.log("%c Connection to  set! \n Overseer library is:", "background: skyblue")
+console.log({
+    overseerLibrary: window.overseerLibrary,
+})
+
+let xxx = 1;
+const yyy = 2;
+var zzz = 3;
+this.aaa = 4;
+
+const config = {};
